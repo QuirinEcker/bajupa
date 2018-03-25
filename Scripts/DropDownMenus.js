3626f28b88e354ed1671011f6aba0a22ea3e85e7
QuirinEcker <quirin.ecker@bajupa.com>
2018-03-25 05:43:00 +0200

    finished menubar

---
+++ Scripts/DropDownMenus.js
@@ -0,0 +1,47 @@
+function dropDownTableDisplay() {
+	let content = document.getElementById("DropDownTable");
+	
+	content.style.height = "150px"
+}
+
+function dropDownTableNone() {
+	let content = document.getElementById("DropDownTable");
+	
+	content.style.height = "0px";
+}
+
+function dropDownProjectsDisplay() {
+	let content = document.getElementById("DropDownProjects");
+	
+	content.style.height = "150px";
+}
+
+function dropDownProjectsNone() {
+	let content = document.getElementById("DropDownProjects");
+	
+	content.style.height = "0px";
+}
+
+function dropDownAboutDisplay() {
+	let content = document.getElementById("DropDownAbout");
+	
+	content.style.height = "150px";
+}
+
+function dropDownAboutNone() {
+	let content = document.getElementById("DropDownAbout");
+	
+	content.style.height = "0px";
+}
+
+function dropDownLinksDisplay(){
+	let content = document.getElementById("DropDownLinks");
+	
+	content.style.height = "150px";
+}
+
+function dropDownLinksNone() {
+	let content = document.getElementById("DropDownLinks");
+	
+	content.style.height = "0px";
+}
