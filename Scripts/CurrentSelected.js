fdc49bab59322da10962608879faf73da8b9e839
QuirinEcker <quirin.ecker@bajupa.com>
2018-03-27 16:06:36 +0200

    adding Aaron Table

--- Scripts/CurrentSelected.js
+++
@@ -1,5 +0,0 @@
-function currentSelected(numberofClass) {
-	let button = document.getElementsByClassName("currentIdSelection");
-	
-    button[numberofClass].style.color = "red";
-}
---
+++ Scripts/Table.js
@@ -0,0 +1,75 @@
+function currentSelected(numberofClass) {
+	let button = document.getElementsByClassName("currentIdSelection");
+	
+	button[numberofClass].style.color = "red";
+	
+	if(numberofClass === 0) {
+		button[1].style.color = "white";
+		button[2].style.color = "white";
+		button[3].style.color = "white";
+		displayCalenderTable()
+	} else if(numberofClass === 1) {
+		button[0].style.color = "white";
+		button[2].style.color = "white";
+		button[3].style.color = "white";
+		displayPeterTable();
+	} else if(numberofClass === 2) {
+		button[0].style.color = "white";
+		button[1].style.color = "white";
+		button[3].style.color = "white";
+		displayAaronTable();
+	} else if(numberofClass === 3) {
+		button[0].style.color = "white";
+		button[1].style.color = "white";
+		button[2].style.color = "white";
+		displayQuirinTable();
+	}
+}
+
+function displayCalenderTable() {
+	let quirinTable = document.getElementById("QuirinStundenPlan");
+	let aaronTable = document.getElementById("AaronStundenPlan");
+	let peterTable = document.getElementById("PeterStundenPlan");
+	let calenderTable = document.getElementById("CalenderStundenPlan");
+	
+	calenderTable.style.display = "block";
+	quirinTable.style.display = "none";
+	aaronTable.style.display = "none";
+	peterTable.style.display = "none"
+}
+
+function displayQuirinTable() {
+	let quirinTable = document.getElementById("QuirinStundenPlan");
+	let aaronTable = document.getElementById("AaronStundenPlan");
+	let peterTable = document.getElementById("PeterStundenPlan");
+	let calenderTable = document.getElementById("CalenderStundenPlan");
+	
+	calenderTable.style.display = "none";
+	quirinTable.style.display = "block";
+	aaronTable.style.display = "none";
+	peterTable.style.display = "none"
+}
+
+function displayAaronTable() {
+	let quirinTable = document.getElementById("QuirinStundenPlan");
+	let aaronTable = document.getElementById("AaronStundenPlan");
+	let peterTable = document.getElementById("PeterStundenPlan");
+	let calenderTable = document.getElementById("CalenderStundenPlan");
+
+	calenderTable.style.display = "none";
+	quirinTable.style.display = "none";
+	aaronTable.style.display = "block";
+	peterTable.style.display = "none"
+}
+
+function displayPeterTable() {
+	let quirinTable = document.getElementById("QuirinStundenPlan");
+	let aaronTable = document.getElementById("AaronStundenPlan");
+	let peterTable = document.getElementById("PeterStundenPlan");
+	let calenderTable = document.getElementById("CalenderStundenPlan");
+
+	calenderTable.style.display = "none";
+	quirinTable.style.display = "none";
+	aaronTable.style.display = "none";
+	peterTable.style.display = "block"
+}
